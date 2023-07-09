8e22ac5ebcf393aa209f2eb98a228a842425419a
Faradina Anggraeni <faradinamaroef@gmail.com>
2023-07-09 14:53:17 +0700

    input database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index(){
-        return view('dashboard.products.index');
+        $data = product::all();
+        return view('dashboard.products.index',compact('data'));
     }
     public function create(){
         return view('dashboard.products.create');
+    }
+    public function insertproduct(Request $request){
+        dd($request->all());
+        product::create($request->all());
+        return redirect()->route('dashboard.product.index');
     }
     public function edit(){
         return view('dashboard.products.edit');
@@ -20,6 +27,11 @@ class ProductController extends Controller
     }
     public function createvariation(){
         return view('dashboard.products.createvariation');
+    }
+    public function insertvariation(Request $request){
+        dd($request->all());
+        product::create($request->all());
+        // return redirect()->route('dashboard.product.variation');
     }
     public function editvariation(){
         return view('dashboard.products.editvariation');
