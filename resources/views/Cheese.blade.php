b3dff83a039dd6476b188aea6a349a209ec22ae3
Faradina Anggraeni <faradinamaroef@gmail.com>
2023-07-13 08:24:08 +0700

    update  api wa

--- resources/views/Cheese.blade.php
+++ resources/views/Cheese.blade.php
@@ -22,17 +22,17 @@
                                 <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
                                     <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
                                     <div
-                                        onclick="window.open(`https://api.whatsapp.com/send/?phone=6281230610228&text&type=phone_number&app_absent=0`)">
+                                        onclick="window.open(`https://api.whatsapp.com/send?phone=6281230610228&text=Halo%20saya%20mau%20pesan%20popcorn%20cheese%20flavour%20100gr`)">
                                         <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
                                         <label class="btn btn-outline-warning" for="btncheck3">100gr</label>
                                     </div>
                                     <div
-                                            onclick="window.open(`https://api.whatsapp.com/send/?phone=6281230610228&text&type=phone_number&app_absent=0`)">
+                                            onclick="window.open(`https://api.whatsapp.com/send?phone=6281230610228&text=Halo%20saya%20mau%20pesan%20popcorn%20cheese%20flavour%20200gr`)">
                                             <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
                                             <label class="btn btn-outline-warning" for="btncheck3">200gr</label>
                                     </div>
                                     <div
-                                        onclick="window.open(`https://api.whatsapp.com/send/?phone=6281230610228&text&type=phone_number&app_absent=0`)">
+                                        onclick="window.open(`https://api.whatsapp.com/send?phone=6281230610228&text=Halo%20saya%20mau%20pesan%20popcorn%20cheese%20flavour%20400gr`)">
                                         <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
                                         <label class="btn btn-outline-warning" for="btncheck3">400 gr</label>
                                     </div>
