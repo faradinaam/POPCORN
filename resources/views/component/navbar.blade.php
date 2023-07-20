41372f637b02d2f3438a68ed4b7921bce6edce00
Faradina Anggraeni <faradinamaroef@gmail.com>
2023-07-20 21:47:59 +0700

    add href product

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -48,7 +48,7 @@
             <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                 <div class="navbar-nav ml-auto">
                     <a href="/" class="nav-item nav-link {{ $title === 'Home' ? 'active' : '' }}">Home</a>
-                    <a href="http://127.0.0.1:8000/#ourProduct" class="nav-item nav-link {{ $title === 'Home' ? 'active' : '' }}">Product</a>
+                    <a href="#ourProduct" class="nav-item nav-link {{ $title === 'Home' ? 'active' : '' }}">Product</a>
                     {{-- <a href="http://127.0.0.1:8000/#Testimoni"class="nav-item nav-link {{ $title === 'Home' ? 'active' : '' }}">Testimoni</a> --}}
                     <a href="/about" class="nav-item nav-link {{ $title === 'About' ? 'active' : '' }}">About</a>
                     @auth
